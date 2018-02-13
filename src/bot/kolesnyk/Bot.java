c81946fa404d9ee1a922960dd8e9c1d0d7cdc918
Yevhenii Kolesnyk <z_kolesnyk@ukr.net>
2018-02-13 18:47:59 +0200

    added reply button & sendPth method

--- src/bot/kolesnyk/Bot.java
+++ src/bot/kolesnyk/Bot.java
@@ -2,11 +2,14 @@ package bot.kolesnyk;
 
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.GetMe;
 import org.telegram.telegrambots.api.methods.send.SendLocation;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.api.objects.Location;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.inlinequery.InlineQuery;
 import org.telegram.telegrambots.api.objects.replykeyboard.ForceReplyKeyboard;
 import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboard;
 import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
@@ -15,6 +18,9 @@ import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 import org.telegram.telegrambots.exceptions.TelegramApiValidationException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Bot extends TelegramLongPollingBot {
@@ -33,14 +39,19 @@ public class Bot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         // костяк с ответами на сообщения
         Message message = update.getMessage();
+
         String text = message.getText();
         if (message.hasText()) {
             switch (text) {
                 case "/start":
-                    sendMsg(message, "Привет. Это простой бот с одной командой /start");
+                    sendMsg(message, "Привет. ");
+                    sendMsg(message, "Ты можешь поделиться с ним своим местоположением, введя команду /loc и жмякнув на кнопку \"отправить геопозицию\". Правда, пока я не придумал, как его можно использовать.");
                     break;
                 case "/loc":
                     sendLoc(message, "Нажмите на кнопку, что бы отправить свои координаты");
+                    break;
+                case "/father":
+                    sendPht(message);
                     break;
                 default:
                     sendMsg(message, String.valueOf(update.getUpdateId()));
@@ -74,21 +85,49 @@ public class Bot extends TelegramLongPollingBot {
     }
 
     @SuppressWarnings("deprecation")
+    private void sendPht(Message msg) {
+        SendPhoto photo = new SendPhoto();
+        photo.setCaption("Твой господин.");
+        photo.setChatId(msg.getChatId());
+        photo.setPhoto("https://pp.userapi.com/c840431/v840431484/2a926/anY0ALawqH4.jpg");
+
+        try {
+            sendPhoto(photo);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("deprecation")
     private void sendLoc(Message msg, String text) {
         SendMessage message = new SendMessage();
         message.enableMarkdown(true);
 
-        KeyboardButton reply = new KeyboardButton();
-        reply.setRequestLocation(true);
-        reply.setText("отправить геопозицию");
+        KeyboardButton keyboardButton = new KeyboardButton();
+        keyboardButton.setRequestLocation(true);
+        keyboardButton.setText("отправить геопозицию");
+
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        List<KeyboardRow> keyboard = new ArrayList<>();
+
+        KeyboardRow buttons = new KeyboardRow();
+        buttons.add(keyboardButton);
+
+        keyboard.add(buttons);
+        replyKeyboardMarkup.setKeyboard(keyboard);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(true);
+        replyKeyboardMarkup.setSelective(true);
 
         message.setChatId(msg.getChatId());
         message.setText(text);
+        message.setReplyMarkup(replyKeyboardMarkup);
 
-        try { // чтобы не крашнулась
+        try {
             sendMessage(message);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
+
     }
 }
