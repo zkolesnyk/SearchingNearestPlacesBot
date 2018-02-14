c7227d9ad82b160e1b2be8c3cc09f36d4fe145f2
Yevhenii Kolesnyk <z_kolesnyk@ukr.net>
2018-02-14 12:23:38 +0200

    added class config

---
+++ src/bot/kolesnyk/Config.java
@@ -0,0 +1,32 @@
+package bot.kolesnyk;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Config {
+    public static final String CONFIG_BOT_FILE = "./config/bot/bot.properties";
+
+    public static String BOT_NAME;
+    public static String BOT_TOKEN;
+
+    public static String DB_PATH;
+    public static String DB_USER;
+    public static String DB_PWD;
+
+    public static void load() {
+        Properties botSettings = new Properties();
+
+        try (InputStream is = new FileInputStream(new File(CONFIG_BOT_FILE))) {
+            botSettings.load(is);
+            is.close();
+            System.out.println("Настройки бота успешно загружены");
+        } catch (Exception e) {
+            System.out.println("Ошибка загрузки настроек бота");
+        }
+
+        BOT_NAME = botSettings.getProperty("botName", "kolesnyk_bot");
+        BOT_TOKEN = botSettings.getProperty("botToken", "Ha-ha-ha");
+    }
+}
