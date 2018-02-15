0e9c4f608e078c21f61c6f332bfb097a4a86320b
Yevhenii Kolesnyk <z_kolesnyk@ukr.net>
2018-02-15 19:16:19 +0200

    add db

--- src/bot/kolesnyk/Config.java
+++ src/bot/kolesnyk/Config.java
@@ -7,9 +7,15 @@ import java.util.Properties;
 
 public class Config {
     public static final String CONFIG_BOT_FILE = "./config/bot/bot.properties";
+    public static final String CONFIG_DB_FILE = "./config/database/database.properties";
 
     public static String BOT_NAME;
     public static String BOT_TOKEN;
+    public static String GOOGLE_PLACES_API_TOKEN;
+
+    public static String DB_URL;
+    public static String DB_USER;
+    public static String DB_PWD;
 
     public static void load() {
         Properties botSettings = new Properties();
@@ -22,7 +28,22 @@ public class Config {
             System.out.println("Ошибка загрузки настроек бота");
         }
 
+        Properties dbSettings = new Properties();
+
+        try (InputStream is = new FileInputStream(new File(CONFIG_DB_FILE))) {
+            dbSettings.load(is);
+            is.close();
+            System.out.println("Настройки БД успешно загружены");
+        } catch (Exception e) {
+            System.out.println("Ошибка загрузки настроек БД");
+        }
+
         BOT_NAME = botSettings.getProperty("botName", "kolesnyk_bot");
         BOT_TOKEN = botSettings.getProperty("botToken", "Ha-ha-ha");
+        GOOGLE_PLACES_API_TOKEN = botSettings.getProperty("googlePlacesApiToken", "Ha-ha-ha");
+
+        DB_URL = dbSettings.getProperty("dbUrl", "");
+        DB_USER = dbSettings.getProperty("dbUser", "");
+        DB_PWD = dbSettings.getProperty("dbPwd", "");
     }
 }
